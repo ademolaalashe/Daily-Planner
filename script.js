dedb3faa3cdf3b4c17178ca857950f69828b64e8
ademolaalashe <77229886+ademolaalashe@users.noreply.github.com>
2023-02-01 22:05:21 +0000

    Add function to update styles

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 const today = moment().format('Do MMM YYYY');
 $('#currentDay').text(today);
 
+// Function to update the styles based on time
+
+function rowColour() {
+    let now = moment().format("HH");
+    $(".row").each(function() {
+        let TimeBlock = moment($(this).find(".time-block").text(), "HH:00").format("HH");
+    })
+}
