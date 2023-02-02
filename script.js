167430d5ac645ab1a7ea9705791505c586ffc689
ademolaalashe <77229886+ademolaalashe@users.noreply.github.com>
2023-02-02 18:47:39 +0000

    Add code to show greetings changes depending on the time of day

--- script.js
+++ script.js
@@ -2,11 +2,45 @@
 const today = moment().format('Do MMM YYYY');
 $('#currentDay').text(today);
 
+// Test code below: Greetings changes depending on the time of day
+
+// let today = new Date();
+// let hourNow = today.getHours();
+// let greetings;
+
+// if (hourNow > 18) {
+//     greeting = "Good evening!";
+// } else if (hourNow > 12) {
+//     greeting = "Good afternoon!";
+// } else if (hourNow > 0) {
+//     greeting = "Good morning!";
+// } else {
+//     greeting = "Welcome!"
+// }
+
+// document.write("<h3>" + greeting + "</h3>");
+
 // Function to update the styles based on time
 
 function rowColour() {
     let now = moment().format("HH");
     $(".row").each(function() {
-        let TimeBlock = moment($(this).find(".time-block").text(), "HH:00").format("HH");
+        let timeBlock = moment($(this).find(".time-block").text(), "HH:00").format("HH");
+
+        if (timeBlock < now ) {
+            $(".row").addClass("past").removeClass("present future");
+        }
+
+        if (timeBlock === now ) {
+            $(".row").addClass("present").removeClass("past future");
+        }
+
+         if (timeBlock > now ) {
+            $(".row").addClass("future").removeClass("past present");
+        }
+
+
     })
 }
+
+rowColour()
