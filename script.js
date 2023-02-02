0ebf458a60f2a2db12bafa8a877fe8681c143352
ademolaalashe <77229886+ademolaalashe@users.noreply.github.com>
2023-02-02 20:11:09 +0000

    Remove comments

--- script.js
+++ script.js
@@ -2,23 +2,6 @@
 const today = moment().format('Do MMM YYYY');
 $('#currentDay').text(today);
 
-// Test code below: Greetings changes depending on the time of day
-
-// let today = new Date();
-// let hourNow = today.getHours();
-// let greetings;
-
-// if (hourNow > 18) {
-//     greeting = "Good evening!";
-// } else if (hourNow > 12) {
-//     greeting = "Good afternoon!";
-// } else if (hourNow > 0) {
-//     greeting = "Good morning!";
-// } else {
-//     greeting = "Welcome!"
-// }
-
-// document.write("<h3>" + greeting + "</h3>");
 
 // Function to update the styles based on time
 
